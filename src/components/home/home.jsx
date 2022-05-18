54dbb47fd057289aec8cd58ee5450aef9903a484
samuelbatista3rios <samuelbatista3rios@gmail.com>
2022-05-18 20:34:36 -0300

    commit3

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -6,5 +6,6 @@ export default props =>
 subtitle="Projeto ReactJs.">
     <div className="display-4">Bem Vindo!</div>
     <hr />
-    <p className="mb-0">Exemplificação de um cadastro desenvolvido com React!</p>
+    <p className="mb-0">O objetivo desse App foi apenas treinar! Nele simulei um sistema de cadastro que busca dado no back-end e retorna na tela, possibilitando a edição do mesmo.
+    a versão mobile ficou com alguns problemas de responsividade para exibir a lista, Na proxima atualização sera arrumada! xD</p>
 </Main>
