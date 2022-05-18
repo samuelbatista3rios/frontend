282005cbc85a649bf61edc966bf8b6b2e0d3eb7c
samuelbatista3rios <samuelbatista3rios@gmail.com>
2022-05-18 20:01:21 -0300

    React Crud

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -17,7 +17,15 @@ const initialState ={
     list:[]
 }
 export default class UserCrud extends Component {
-                 state = { ...initialState };
+         
+     state = { ...initialState };
+
+   //consulta pra trazer os usuarios  GET
+        componentWillMount(){
+          axios(baseUrl).then(resp =>{
+            this.setState({list: resp.data})
+          })
+        }
                  clear() {
                    this.setState({ user: initialState.user });
                  }
@@ -31,9 +39,9 @@ export default class UserCrud extends Component {
                      this.setState({ user: initialState.user, list });
                    });
                  }
-                 getUpdateList(user) {
+                 getUpdateList(user, add = true) {
                    const list = this.state.list.filter((u) => u.id !== user.id);
-                   list.unshift(user);
+                  if(add) list.unshift(user);
                    return list;
                  }
                  updateField(event) {
@@ -94,10 +102,59 @@ export default class UserCrud extends Component {
                      </div>
                    );
                  }
+
+                 load(user){
+                   this.setState({ user})
+                 }
+                 remove(user){
+                   axios.delete(`${baseUrl}/${user.id}`).then(resp =>{
+                     const list = this.getUpdateList(user, false)
+                     this.setState({ list})
+                   })
+                 }
+                 renderTable(){
+                   return(
+                     <table className="table mt-4">
+                       <thead>
+                         <tr>
+                         <th>Id</th>
+                           <th>Nome</th>
+                           <th>E-mail</th>
+                           <th>Ações</th>
+                         </tr>
+                       </thead>
+                       <tbody>
+                         {this.renderRows()}
+                       </tbody>
+                     </table>
+                   )
+                 }
+                 renderRows(){
+                   return this.state.list.map(user => {
+                     return(
+                       <tr key={user.id}>
+                         <td>{user.id}</td>
+                         <td>{user.name}</td>
+                         <td>{user.email}</td>
+                         <td>
+                           <button className="btn btn-warning"
+                           onClick={()=> this.load(user)}>
+                             <i className="fa fa-pencil"></i>
+                           </button>
+                           <button className="btn btn-danger ml-2"
+                           onClick={() => this.remove(user)}>
+                             <i className="fa fa-trash"></i>
+                           </button>
+                         </td>
+                       </tr>
+                     )
+                   })
+                 }
                  render() {
                    return (
                    <Main {...headerProps}>
                        {this.renderForm()}
+                       {this.renderTable()}
                        </Main>
                    )
                  }
