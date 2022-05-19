29ecae4fe96f29c9b331ef5848d5f68f7f56d1da
samuelbatista3rios <samuelbatista3rios@gmail.com>
2022-05-19 17:04:45 -0300

    comi

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Main from '../template/Main'
+import {Link} from 'react-router-dom'
 
 export default props =>
 <Main icon="home" title="Início" 
@@ -8,4 +9,8 @@ subtitle="Projeto ReactJs.">
     <hr />
     <p className="mb-0">O objetivo desse App foi apenas treinar! Nele simulei um sistema de cadastro que busca dado no back-end e retorna na tela, possibilitando a edição do mesmo.
     a versão mobile ficou com alguns problemas de responsividade para exibir a lista, Na proxima atualização sera arrumada! xD</p>
+    <hr/>
+    <Link to='https://www.linkedin.com/in/samuel-fonseca-0289a6121/'>
+           <i className='fa fa-linkedin'>Meu Linkedin</i>
+    </Link>
 </Main>
