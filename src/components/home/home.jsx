1d69887f31e9ed2dde24ea6823b7defecc8dde11
samuelbatista3rios <samuelbatista3rios@gmail.com>
2024-07-24 10:47:53 -0300

    comiits

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -8,7 +8,7 @@ subtitle="Projeto ReactJs.">
     <div className="display-4">Bem Vindo!</div>
     <hr />
     <p className="mb-0">O objetivo desse App foi apenas treinar! Nele simulei um sistema de cadastro que busca dado no back-end e retorna na tela, possibilitando a edição do mesmo.
-    a versão mobile ficou com alguns problemas de responsividade para exibir a lista, Na proxima atualização sera arrumada! xD</p>
+    a versão mobile ficou com alguns problemas de responsividade para exibir a lista, Na proxima atualização será arrumada! xD</p>
     <hr/>
     <Link to='https://www.linkedin.com/in/samuel-fonseca-0289a6121/'>
            <i className='fa fa-linkedin'>Meu Linkedin</i>
--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -8,12 +8,14 @@ import Logo from "../template/Logo";
 import Nav from "../template/Nav";
 import Routes from './Routes'
 import Footer from "../template/Footer";
+import Home from "../home/home";
 
 export default (props) => (
   <BrowserRouter>
     <div className="app">
       <Logo />
       <Nav />
+      <Home/>
       <Routes />
       <Footer />
     </div>
